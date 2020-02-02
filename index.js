729dd3348d59f6e4ae9b5229ae60dfa4a6402950
Noah Baltensperger <nbalten@gmail.com>
2020-02-01 21:41:01 -0600

    Update index.js
    updated color change with keypress

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 document.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.btn[data-key="${e.keyCode}"]`)
     if(!audio) return;
+    audio.currentTime = 0;
     audio.play();
-})
+    key.classList.add('keyPress');
+});
+
+function removeTransition(e) {
+    if(e.propertyName != 'transform') return;
+    this.classList.remove('keyPress');
+}
+
+const keys = document.querySelectorAll('.btn');
+keys.forEach(key => key.addEventListener('transitioned', removeTransition));
